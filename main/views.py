534d59a4113d95a678782c6e61eb1b9c0bab86c8
Nadine Aisyah Putri Maharani <nadineaisyah170806@gmail.com>
2025-09-03 15:06:00 +0700

    Tutorial 0: Menghubungkan view dengan template

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+def show_main(request):
+    context = {
+        'npm' : '2406408224',
+        'name': 'Nadine Aisyah Putri Maharani',
+        'class': 'PBP F'
+    }
+
+    return render(request, "main.html", context)
 # Create your views here.
