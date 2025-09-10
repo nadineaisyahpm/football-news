e4334d0313734dc4133bd410bef050450fa1e0ca
Nadine Aisyah Putri Maharani <nadineaisyah170806@gmail.com>
2025-09-10 15:44:41 +0700

    Tutorial 2 Selesai

--- main/views.py
+++ main/views.py
@@ -8,9 +8,9 @@ def show_main(request):
     news_list = News.objects.all()
 
     context = {
-        'npm' : '240123456',
-        'name': 'Haru Urara',
-        'class': 'PBP A',
+        'npm' : '2406408224',
+        'name': 'Nadine Aisyah Putri Maharani',
+        'class': 'PBP F',
         'news_list': news_list
     }
 
